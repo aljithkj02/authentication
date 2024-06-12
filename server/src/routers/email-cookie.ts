198ed929774471a00dd2a7f4142650d879ee931a
aljithkj02 <kwdeathlok@gmail.com>
2024-06-12 14:57:17 +0530

    add openapi doc to email-cookie router

--- server/src/routers/email-cookie.ts
+++ server/src/routers/email-cookie.ts
@@ -3,6 +3,74 @@ import { loginUserCookie, logoutUserCookie, signupUserCookie } from "../controll
 
 export const emailCookieRouter = Router();
 
+/**
+ * @openapi
+ * /api/email-cookie/signup:
+ *   post:
+ *     summary: Signup User
+ *     tags: 
+ *       - Auth-Email-COOKIE
+ *     requestBody: 
+ *       description: User credentials to be added
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *           example:
+ *             name: "Krishna"
+ *             email: "krishna@gmail.com"
+ *             password: "krishna123"
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               data: [{}]
+ *       401:
+ *         description: Unauthorized request
+ */
 emailCookieRouter.post('/signup', signupUserCookie);
+
+/**
+ * @openapi
+ * /api/email-cookie/login:
+ *   post:
+ *     summary: Login User
+ *     tags: 
+ *       - Auth-Email-COOKIE
+ *     requestBody: 
+ *       description: User credentials to be added
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *           example:
+ *             email: "krishna@gmail.com"
+ *             password: "krishna123"
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               data: [{}]
+ *       401:
+ *         description: Unauthorized request
+ */
 emailCookieRouter.post('/login', loginUserCookie);
 emailCookieRouter.get('/logout', logoutUserCookie);
